13b77b8aad31e30996e1cd54bc5f3cf46e46db55
Mikolaj Mirko <mikolajmirko@gmail.com>
2020-05-16 18:27:24 +0200

    Logo animation added, color correction, Jan's picture fixed

--- assets/main.js
+++ assets/main.js
@@ -4,3 +4,12 @@ $('.dropdown')
     .dropdown({
         action: 'hide'
     });
+
+// Animation
+var anim = lottie.loadAnimation({
+    container: document.getElementById('sunflower'),
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'assets/sunflower_with_title.json'
+});
